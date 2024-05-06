79dc82b39e35fe853e81da4db2913e5ff7c9c680
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-05-06 15:28:52 -0300

    :sparkles: added new code version to main

--- DLDP/dldp.lua
+++ DLDP/dldp.lua
@@ -6,12 +6,13 @@ local dldp = Proto("dldp", "Device Link Detection Protocol")
 
 -- Defining the protocol sections
 local fields = dldp.fields
-fields.sequence = ProtoField.bytes("dldp.sequence", "Sequence Number")
+fields.sequence = ProtoField.uint16("dldp.sequence", "Sequence Number")
 fields.mac_src = ProtoField.bytes("dldp.mac_src", "Source MAC")
-fields.interval = ProtoField.bytes("dldp.interval", "DLDP Interval")
-fields.password = ProtoField.bytes("dldp.password", "DLDP Password")
+fields.interval = ProtoField.uint16("dldp.interval", "DLDP Interval")
+fields.password = ProtoField.string("dldp.password", "DLDP Password")
 fields.flags = ProtoField.bytes("dldp.flags", "DLDP Config Flags")
 
+-- Função de dissecação
 function dldp.dissector(buffer, pinfo, tree)
     length = buffer:len()
     if length == 0 then return end
@@ -19,13 +20,14 @@ function dldp.dissector(buffer, pinfo, tree)
     pinfo.cols.protocol = dldp.name
 
     local subtree = tree:add(dldp, buffer(), "DLDP Protocol Data")
-    if buffer(0, 1):uint() == 0x00 then
+
+if buffer(0, 1):uint() == 0x00 then
 
     subtree:add(fields.flags, buffer(2,4))
-    subtree:add(fields.password, buffer(6, 12))
-    subtree:add(fields.interval, buffer(23,1))
+    subtree:add(fields.password, buffer(6, 12):string())
+    subtree:add(fields.interval, buffer(22,2):uint())
     subtree:add(fields.mac_src, buffer(26,6))
-    subtree:add(fields.sequence, buffer(32,2))
+    subtree:add(fields.sequence, buffer(32,2):uint())
   
     pinfo.cols.info:set("DLDP Packet")
     else
