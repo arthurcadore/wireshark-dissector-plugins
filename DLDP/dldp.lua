82c0211d93c2e66b37a182076fd96bcfaf29a1f4
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-05-06 16:13:15 -0300

    :sparkles: updated dldp for dynamic password lenght

--- DLDP/dldp.lua
+++ DLDP/dldp.lua
@@ -4,7 +4,6 @@
 -- Defining the protocol description
 local dldp = Proto("dldp", "Device Link Detection Protocol")
 
--- Defining the protocol sections
 local fields = dldp.fields
 fields.sequence = ProtoField.uint16("dldp.sequence", "Sequence Number")
 fields.mac_src = ProtoField.bytes("dldp.mac_src", "Source MAC")
@@ -12,7 +11,6 @@ fields.interval = ProtoField.uint16("dldp.interval", "DLDP Interval")
 fields.password = ProtoField.string("dldp.password", "DLDP Password")
 fields.flags = ProtoField.bytes("dldp.flags", "DLDP Config Flags")
 
--- Função de dissecação
 function dldp.dissector(buffer, pinfo, tree)
     length = buffer:len()
     if length == 0 then return end
@@ -20,16 +18,23 @@ function dldp.dissector(buffer, pinfo, tree)
     pinfo.cols.protocol = dldp.name
 
     local subtree = tree:add(dldp, buffer(), "DLDP Protocol Data")
+    if buffer(0, 1):uint() == 0x00 then
+        local password_end = buffer:len() - 4
+        local password_length = 0
+        for i=6, password_end do
+            if buffer(i, 1):uint() == 0x00 and buffer(i+1, 1):uint() == 0x00
+               and buffer(i+2, 1):uint() == 0x00 and buffer(i+3, 1):uint() == 0x00 then
+                password_length = i - 6
+                break
+            end
+        end
 
-if buffer(0, 1):uint() == 0x00 then
-
-    subtree:add(fields.flags, buffer(2,4))
-    subtree:add(fields.password, buffer(6, 12):string())
-    subtree:add(fields.interval, buffer(22,2):uint())
-    subtree:add(fields.mac_src, buffer(26,6))
-    subtree:add(fields.sequence, buffer(32,2):uint())
-  
-    pinfo.cols.info:set("DLDP Packet")
+        subtree:add(fields.flags, buffer(2,4))
+        subtree:add(fields.password, buffer(6, password_length):string())
+        subtree:add(fields.interval, buffer(6 + password_length + 4,2):uint())
+        subtree:add(fields.mac_src, buffer(6 + password_length + 8,6))
+        subtree:add(fields.sequence, buffer(6 + password_length + 14,2):uint())
+        pinfo.cols.info:set("DLDP Packet")
     else
         pinfo.cols.info:set("General-Slow-Protocol-Packet")
     end
